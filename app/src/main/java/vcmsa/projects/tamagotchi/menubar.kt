7283ff91a4faa862976869cab8794565088a617a
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-14 20:50:33 +0200

    Correct association with bar graph

--- app/src/main/java/vcmsa/projects/tamagotchi/menubar.kt
+++ app/src/main/java/vcmsa/projects/tamagotchi/menubar.kt
@@ -1,8 +1,10 @@
 package vcmsa.projects.tamagotchi
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,14 +17,19 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.components.XAxis
+
 
 private val FragInit = FragmentDisplay()
 private val FragSleep = FragmentSleep()
 private val FragEat = FragmentEat()
 private val FragRead = FragmentRead()
 private val FragPaint = FragmentPaint()
+private val FragDeceased = FragmentDeceased()
 
 var statusArrayList = arrayListOf<BarEntry>()
+private val barLabels = listOf("Null", "Sleep", "Paint", "Read", "Eat")
 
 class menubar : AppCompatActivity() {
     private lateinit var barChart: HorizontalBarChart
@@ -44,17 +51,19 @@ class menubar : AppCompatActivity() {
 
         statusArrayList = arrayListOf<BarEntry>()
         statusArrayList.add( BarEntry(2f,10f))
-        statusArrayList.add( BarEntry(3f,20f))
-        statusArrayList.add( BarEntry(4f,5f))
-        statusArrayList.add( BarEntry(5f,15f))
+        statusArrayList.add( BarEntry(3f,10f))
+        statusArrayList.add( BarEntry(4f,10f))
+        statusArrayList.add( BarEntry(5f,10f))
 
         statusArrayList = arrayListOf(
             BarEntry(1f, 10f), // sleep
-            BarEntry(2f, 15f), // eat
-            BarEntry(3f, 20f), // read
-            BarEntry(4f, 25f)  // paint
+            BarEntry(2f, 10f), // eat
+            BarEntry(3f, 10f), // read
+            BarEntry(4f, 10f)  // paint
         )
         updateBarChart()
+        barTracker()
+
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.NavBar)
         bottomNav.setOnItemSelectedListener { item ->
@@ -100,7 +109,21 @@ class menubar : AppCompatActivity() {
             transaction.commit()
         }
     }
+private fun barTracker(){
+    val sleepValue = statusArrayList[0].y
+    val eatValue = statusArrayList[1].y
+    val readValue = statusArrayList[2].y
+    val paintValue = statusArrayList[3].y
 
+    if (sleepValue == 0f || eatValue == 0f || readValue ==0f || paintValue==0f)
+    {
+        val intent = Intent(this,FragmentDeceased::class.java)
+        startActivity(intent)
+        Toast.makeText(this, "You neglected your penguin! GAME OVER", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
     private fun updateBarChart() {
         val barDataSet = BarDataSet(statusArrayList, "Status")
         barDataSet.colors = ColorTemplate.COLORFUL_COLORS.toList()
@@ -111,6 +134,15 @@ class menubar : AppCompatActivity() {
         barChart.data = barData
         barChart.setFitBars(true)
         barChart.invalidate() // Refresh the chart
+
+        val xAxis = barChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(barLabels)
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.granularity = 1f
+        xAxis.labelCount = barLabels.size
+        xAxis.textColor = Color.BLACK
+
     }
 
 
